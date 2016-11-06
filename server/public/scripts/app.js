ebe8cd660685ee4af4f7995a50624436f484e5f6
Brian Matney <brianamatney@gmail.com>
2016-11-06 15:49:32 -0600

    I should have done this to begin with

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,24 @@
-$(document).ready(function(){
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        console.log(data);
-      }
-    });
-});
+$(document).ready(function () {
+
+    $("#sigmaRoster").on("click", getAjax);
+
+    function getAjax() {
+      $.ajax({
+        type: "GET",
+        url: "/data",
+        success: function (data) {
+            console.log(data);
+            updateDom(data.sigmanauts);
+          },
+      });
+    }
+
+    function updateDom(sigmanauts) {
+      $.each(sigmanauts, function (i, student) {
+        $("#sigma-container").append('<div class="indSigma"></div>');
+        var $el = $("#sigma-container").children().last();
+
+        $el.append('<h2>' + student.name + '</h2>');
+      });
+    }
+  });
