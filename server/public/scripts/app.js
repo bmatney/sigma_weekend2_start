2f5cf13e2eb30901aa7dc68209e77414fba74f65
Brian Matney <brianamatney@gmail.com>
2016-11-06 17:52:23 -0600

    previous button working

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,24 +1,46 @@
 var i = 1;
 
 $(document).ready(function () {
-  $("#next").on("click", getAjax);
+  $("#next").on("click", nextStudent);
+  $("#previous").on("click", previousStudent);
 
 });
 
-function getAjax() {
+function nextStudent() {
   $.ajax({
         type: "GET",
         url: "/data",
         success: function (data) {
-            updateDom(data.sigmanauts);
+            nextButton(data.sigmanauts);
           },
       });
 }
 
-function updateDom(sigmanauts) {
+function nextButton(sigmanauts) {
+  if (i > 19) {
+    i = 19;
+  }
   $("h2").replaceWith('<h2>' + sigmanauts[i].name + '</h2>');
   $("a").replaceWith('<a href=https://github.com/' + sigmanauts[i].git_username + '>' + "Check out my Github!" + '</a>');
   $("p").replaceWith('<p>' + sigmanauts[i].shoutout + '</p>');
   i++;
-  console.log('#sigma-container');
+}
+
+function previousStudent() {
+  $.ajax({
+        type: "GET",
+        url: "/data",
+        success: function (data) {
+            previousAction(data.sigmanauts);
+          },
+      });
+}
+function previousAction(sigmanauts) {
+  if (i < 1) {
+    i = 1;
+  }
+    i--;
+  $("h2").replaceWith('<h2>' + sigmanauts[i].name + '</h2>');
+  $("a").replaceWith('<a href=https://github.com/' + sigmanauts[i].git_username + '>' + "Check out my Github!" + '</a>');
+  $("p").replaceWith('<p>' + sigmanauts[i].shoutout + '</p>');
 }
