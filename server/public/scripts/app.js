d20daf49171b8fae86785ed6861cf0e5744da74d
Brian Matney <brianamatney@gmail.com>
2016-11-07 08:55:40 -0600

    base assignment complete

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,10 +1,11 @@
 // variables
 
-var i = 1;
+var i = 0;
 
 // When Page loads
 
 $(document).ready(function () {
+
   // buildIndex();
   $("#next").on("click", nextStudent);
   $("#previous").on("click", previousStudent);
@@ -19,22 +20,24 @@ function nextStudent() {
     type: "GET",
     url: "/data",
     success: function (data) {
-      nextButton(data.sigmanauts);
+        nextButton(data.sigmanauts);
       },
   });
 }
 
 function nextButton(sigmanauts) {
-  $('#sigma-container').fadeOut(function(){
-  $("h2").replaceWith('<h2>' + sigmanauts[i].name + '</h2>');
-  $("a").replaceWith('<a href=https://github.com/' + sigmanauts[i].git_username + '>' + "Check out my Github!" + '</a>');
-  $("p").replaceWith('<p>' + sigmanauts[i].shoutout + '</p>');
   i++;
   if (i > 19) {
     i = 19;
   }
-  $('#sigma-container').fadeIn();
-});
+
+  $('#sigma-container').fadeOut(function () {
+    $("h2").replaceWith('<h2>' + sigmanauts[i].name + '</h2>');
+    $("a").replaceWith('<a href=https://github.com/' + sigmanauts[i].git_username + '>' + "Check out my Github!" + '</a>');
+    $("p").replaceWith('<p>' + sigmanauts[i].shoutout + '</p>');
+    highlightSelected();
+    $('#sigma-container').fadeIn();
+  });
 }
 
 // Functions for button for previous student
@@ -50,16 +53,18 @@ function previousStudent() {
 }
 
 function previousButton(sigmanauts) {
-  $('#sigma-container').fadeOut(function(){
   i--;
-  $("h2").replaceWith('<h2>' + sigmanauts[i].name + '</h2>');
-  $("a").replaceWith('<a href=https://github.com/' + sigmanauts[i].git_username + '>' + "Check out my Github!" + '</a>');
-  $("p").replaceWith('<p>' + sigmanauts[i].shoutout + '</p>');
-  if (i < 1) {
-    i = 1;
+  if (i < 0) {
+    i = 0;
   }
-  $('#sigma-container').fadeIn();
-});
+
+  $('#sigma-container').fadeOut(function () {
+    $("h2").replaceWith('<h2>' + sigmanauts[i].name + '</h2>');
+    $("a").replaceWith('<a href=https://github.com/' + sigmanauts[i].git_username + '>' + "Check out my Github!" + '</a>');
+    $("p").replaceWith('<p>' + sigmanauts[i].shoutout + '</p>');
+    highlightSelected();
+    $('#sigma-container').fadeIn();
+  });
 }
 
 // function foor making index
@@ -73,9 +78,14 @@ function makeIndex() {
   });
 }
 
-function buildIndex(sigmanauts){
+function buildIndex(sigmanauts) {
   for (var i = 0; i < sigmanauts.length; i++) {
     $('#classIndex').append('<div class="stuff" id="index' + i + '"></div>');
-    $('#index'+i).text(i + 1);
+    $('#index' + i).text(i + 1);
   }
+}
+
+function highlightSelected() {
+  $('.stuff').css("background-color", "white")
+  $('#index' + i).css("background-color", "cyan")
 }
